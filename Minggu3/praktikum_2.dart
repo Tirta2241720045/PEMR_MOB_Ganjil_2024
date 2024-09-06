6cb0c0bed0e34abbd92e94723d18bd13f3fcb6ba
Tirta Nurrochman Bintang Prawira <2241720045@student.polinema.ac.id>
2024-09-06 10:29:37 +0700

    praktikum2

--- Minggu3/praktikum_2.dart
+++ Minggu3/praktikum_2.dart
@@ -1,1 +1,11 @@
-void main() {}
+void main() {
+  int counter = 0;
+  while (counter < 33) {
+    print(counter);
+    counter++;
+  }
+  do {
+    print(counter);
+    counter++;
+  } while (counter < 77);
+}
