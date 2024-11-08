8fb0538d0a112512db1964fc2572a73e0b201828
Tirta Nurrochman Bintang Prawira <2241720045@student.polinema.ac.id>
2024-11-08 10:19:11 +0700

    Start

---
+++ Minggu9/master_plan/lib/views/plan_screen.dart
@@ -0,0 +1,101 @@
+import '../models/data_layer.dart';
+import 'package:flutter/material.dart';
+
+class PlanScreen extends StatefulWidget {
+  const PlanScreen({super.key});
+
+  @override
+  State createState() => _PlanScreenState();
+}
+
+class _PlanScreenState extends State<PlanScreen> {
+  Plan plan = const Plan(); // Pastikan Plan memiliki konstruktor yang benar
+  late ScrollController scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController()
+      ..addListener(() {
+        FocusScope.of(context).requestFocus(FocusNode());
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Master Plan Namaku Rochmen')),
+      body: _buildList(),
+      floatingActionButton: _buildAddTaskButton(),
+    );
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  Widget _buildAddTaskButton() {
+    return FloatingActionButton(
+      child: const Icon(Icons.add),
+      onPressed: () {
+        setState(() {
+          plan = Plan(
+            name: plan.name,
+            tasks: List<Task>.from(plan.tasks)
+              ..add(const Task()), // Penambahan Task baru
+          );
+        });
+      },
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.builder(
+      controller: scrollController,
+      itemCount: plan.tasks.length, // Pastikan jumlah item ter-update
+      itemBuilder: (context, index) {
+        return _buildTaskTile(plan.tasks[index], index);
+      },
+      keyboardDismissBehavior: Theme.of(context).platform == TargetPlatform.iOS
+          ? ScrollViewKeyboardDismissBehavior.onDrag
+          : ScrollViewKeyboardDismissBehavior.manual,
+    );
+  }
+
+  Widget _buildTaskTile(Task task, int index) {
+    return ListTile(
+      leading: Checkbox(
+        value: task.complete,
+        onChanged: (selected) {
+          setState(() {
+            plan = Plan(
+              name: plan.name,
+              tasks: List<Task>.from(plan.tasks)
+                ..[index] = Task(
+                  description: task.description,
+                  complete: selected ?? false,
+                ),
+            );
+          });
+        },
+      ),
+      title: TextFormField(
+        initialValue: task.description,
+        onChanged: (text) {
+          setState(() {
+            plan = Plan(
+              name: plan.name,
+              tasks: List<Task>.from(plan.tasks)
+                ..[index] = Task(
+                  description: text,
+                  complete: task.complete,
+                ),
+            );
+          });
+        },
+      ),
+    );
+  }
+}
